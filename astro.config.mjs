40febb64b2413856a7cdcfc855ae59ae868f76d7
andersseen <andriipap01@gmail.com>
2025-12-03 18:07:04 +0100

    feat: add path aliases for components and scripts; refactor imports across the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 import tailwindcss from "@tailwindcss/vite";
 import preact from "@astrojs/preact";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,5 +25,17 @@ export default defineConfig({
   },
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@componentes": `${__dirname}/src/components`,
+        "@i18n": `${__dirname}/src/i18n`,
+        "@layouts": `${__dirname}/src/layouts`,
+        "@pages": `${__dirname}/src/pages`,
+        "@styles": `${__dirname}/src/styles`,
+        "@scripts": `${__dirname}/src/scripts`,
+        "@assets": `${__dirname}/src/assets`,
+        "@": `${__dirname}/src`,
+      },
+    },
   },
 });
